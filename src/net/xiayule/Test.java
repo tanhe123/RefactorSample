8ae0d696ae0aa6ea42d3825efb7fb8b3a5be0d0f
Hehe Tan <xiayule148@gmail.com>
2014-11-30 16:43:23 +0800

    编写测试代码

--- src/net/xiayule/Test.java
+++ src/net/xiayule/Test.java
@@ -6,12 +6,25 @@ package net.xiayule;
 public class Test {
     public static void main(String[] args) {
 
+        Movie 射雕英雄传 = new Movie("射雕英雄传", 0);
+        Movie 海面宝宝 = new Movie("海面宝宝", 2);
+        Movie 大长今 = new Movie("大长今", 1);
 
-        Customer customer = new Customer("tan");
-        customer.addRental(new Rental(new Movie("小", 1), 3));
+        Customer 小明 = new Customer("小明");
+
+        小明.addRental(new Rental(射雕英雄传, 2));
+        小明.addRental(new Rental(海面宝宝, 4));
+        小明.addRental(new Rental(大长今, 1));
 
 
 
-        System.out.println(customer.statement());
+        String result = "Rental Record for 小明\n" +
+                "\t射雕英雄传\t2.0\n" +
+                "\t海面宝宝\t3.0\n" +
+                "\t大长今\t3.0\n" +
+                "Amount owed is 8.0\n" +
+                "You earned 3 frequent renter points";
+
+        System.out.println(小明.statement().equals(result));
     }
 }
